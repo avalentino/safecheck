96259ba221bef4eb24bdd147c729afd10761c7f9
Antonio Valentino <Antonio.Valentino@ext.esa.int>
2023-03-13 20:06:26 +0100

    Reduce deltas wrt the official version in main

--- safecheck.py
+++ safecheck.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-# PYTHON_ARGCOMPLETE_OK
-# Copyright (C) 2011-2012 S[&]T, The Netherlands.
 
 """Perform consistency checks on SAFE products.
 
@@ -15,7 +13,6 @@ Additional checks on consistency between the product name and information
 included in the manifest file are also performed.
 """
 
-import io
 import os
 import sys
 import hashlib
@@ -24,7 +21,7 @@ import pathlib
 import argparse
 import binascii
 import functools
-from typing import IO, Iterator, Optional, Union
+from typing import Optional, Union
 
 from lxml import etree
 
@@ -36,19 +33,11 @@ __copyright__ = 'Copyright (C) 2011-2023 S[&]T, The Netherlands.'
 __version__ = '3.0'
 
 
-EX_OK = getattr(os, "EX_OK", 0)
-EX_FAILURE = 1
-EX_ERROR = 2
-EX_WARNING = 3
-EX_INTERRUPT = 130
-
-
 NSXFDU = "{urn:ccsds:schema:xfdu:1}"
 
-
-MANIFEST_SCHEMA_STR = """\
-<?xml version="1.0"?>
-<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xfdu="urn:ccsds:schema:xfdu:1" targetNamespace="urn:ccsds:schema:xfdu:1" elementFormDefault="unqualified" attributeFormDefault="unqualified">
+MANIFEST_SCHEMA_STR = """<?xml version="1.0"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xfdu="urn:ccsds:schema:xfdu:1"
+    targetNamespace="urn:ccsds:schema:xfdu:1" elementFormDefault="unqualified" attributeFormDefault="unqualified">
   <xs:simpleType name="locatorTypeType">
     <xs:restriction base="xs:string">
       <xs:enumeration value="URL"/>
@@ -314,63 +303,54 @@ MANIFEST_SCHEMA_STR = """\
 """
 
 
+XmlSchemaType = Union[str, os.PathLike]
+
+
+def check_file_against_schema(xmlfile: os.PathLike,
+                              schema: XmlSchemaType) -> bool:
+    """Validate the input XML file aganst the provided schema."""
+    if schema.startswith('<?xml'):
+        xmlschema = etree.XMLSchema(etree.fromstring(schema))
+        schema = "built-in schema"
+    else:
+        xmlschema = etree.XMLSchema(etree.parse(os.fspath(schema)).getroot())
+    try:
+        xmlschema.assertValid(etree.parse(os.fspath(xmlfile)))
+    except etree.DocumentInvalid as exc:
+        logger.error(f"could not verify '{xmlfile}' against schema '{schema}'")
+        for error in exc.error_log:
+            logger.error(f"{error.filename}:{error.line}: {error.message}")
+        return False
+    logger.debug(f"file '{xmlfile}' valid according to schema '{schema}'")
+    return True
+
+
 def is_xml(filename: os.PathLike) -> bool:
     """Return True if the filename is an XML file."""
     filename = pathlib.Path(filename)
     return filename.suffix.lower() == '.xml' and filename.name[0] != "."
 
 
-def crc16(data: bytes, crc: int = 0xFFFF) -> bytes:
-    """Compute CRC-16 incrementally."""
-    return binascii.crc_hqx(data, crc)
-
-
-def blockiter(fd: IO, blocksize: int = io.DEFAULT_BUFFER_SIZE) -> Iterator:
-    """Iterate on file-like objects reading blocks of the specified size.
-
-    The `fd` parameter must be a binary or text file-like object opened
-    for reading.
-    The `blocksize` parameter defaults to `io.DEFAULT_BUFFER_SIZE`.
-    """
-    guard = '' if isinstance(fd, io.TextIOBase) else b''
-    return iter(functools.partial(fd.read, blocksize), guard)
-
-
-def get_md5sum(filename: os.PathLike) -> str:
+def md5sum(filename: os.PathLike) -> str:
     """Retuen the MD% checksum of the specified file."""
     md5 = hashlib.md5()
     with open(filename, 'rb') as fd:
-        for data in blockiter(fd):
+        for data in iter(functools.partial(fd.read, 65536), b""):
             md5.update(data)
     return md5.hexdigest()
 
 
-XmlSchemaType = Union[os.PathLike, etree.XMLSchema]
+def check_product_crc(product: os.PathLike, manifestfile: os.PathLike) -> bool:
+    """Check the crc16 checksum of the manifest file.
 
-
-def check_file_against_schema(xmlfile: os.PathLike,
-                              schema: XmlSchemaType) -> bool:
-    """Validate the input XML file aganst the provided schema."""
-    xmldoc = etree.parse(os.fspath(xmlfile))
-
-    if not isinstance(schema, etree.XMLSchema):
-        schemafile = schema
-        schemadoc = etree.parse(os.fspath(schemafile))
-        schema = etree.XMLSchema(schemadoc.getroot())
-    else:
-        schemafile = "basic manifest schema (internal)"
-
-    try:
-        schema.assertValid(xmldoc)
-    except etree.DocumentInvalid as exc:
-        logger.error(
-            f"could not verify '{xmlfile}' against schema '{schemafile}'")
-        for error in exc.error_log:
-            logger.error(f"{error.filename}:{error.line}: {error.message}")
-        logger.error(f"{xmlfile} fails to validate")
+    Check if the crc16 checksum of the manifest file is consistent with
+    the one included in the product name.
+    """
+    expected_crc = format(binascii.crc_hqx(pathlib.Path(manifestfile).read_bytes(), 0xFFFF), '04X')
+    actual_crc = pathlib.Path(product).stem[-4:]
+    if expected_crc != actual_crc:
+        logger.warning(f"crc in product name '{actual_crc}' does not match crc of manifest file '{expected_crc}'")
         return False
-
-    logger.info(f"file '{xmlfile}' valid according to schema '{schemafile}'")
     return True
 
 
@@ -378,73 +358,50 @@ def check_manifest_file(filename: os.PathLike,
                         schema: Optional[os.PathLike] = None) -> bool:
     """Check id the manifest file is valid according to the provided schema."""
     if schema is None:
-        schemadoc = etree.fromstring(MANIFEST_SCHEMA_STR)
-        schema = etree.XMLSchema(schemadoc)
+        schema = MANIFEST_SCHEMA_STR
     return check_file_against_schema(filename, schema)
 
 
-def check_product_crc(product: os.PathLike, manifestfile: os.PathLike) -> bool:
-    """Check the crc16 checksum of the manifest file.
-    
-    Check if the crc16 checksum of the manifest file is consistent with
-    the one included in the product name.
-    """
-    product = pathlib.Path(product)
-    manifestfile = pathlib.Path(manifestfile)
-
-    expected_crc = crc16(manifestfile.read_bytes())
-    expected_crc = format(expected_crc, '04X')
-    actual_crc = product.stem[-4:]
-    if expected_crc != actual_crc:
-        logger.warning(
-            f"crc in product name '{actual_crc}' does not match crc "
-            f"of manifest file '{expected_crc}'"
-        )
-        return False
-    return True
-
-
 def verify_safe_product(product: os.PathLike,
-                        schema: Optional[os.PathLike] = None) -> int:
+                        manifest_schema: Optional[os.PathLike] = None) -> int:
     """Perform consistency checks of a SAVE products."""
     has_errors = False
     has_warnings = False
 
-    if not pathlib.Path(product).exists():
-        logger.error(f"could not find '{product}'")
-        return EX_ERROR
+    product = pathlib.Path(product)
 
-    product = pathlib.Path(product).resolve()
+    if not product.exists():
+        logger.error(f"could not find '{product}'")
+        return 2
 
     manifestfile = product / "manifest.safe"
     if not manifestfile.exists():
         logger.error(f"could not find '{manifestfile}'")
-        return EX_ERROR
+        return 2
 
     if product.name[4:7] != "AUX":
         if not check_product_crc(product, manifestfile):
             has_warnings = True
 
-    if not check_manifest_file(manifestfile, schema):
+    if not check_manifest_file(manifestfile, manifest_schema):
         has_errors = True
-    manifest_xmldoc = etree.parse(os.fspath(manifestfile))
-    if manifest_xmldoc is None:
+    manifest = etree.parse(os.fspath(manifestfile))
+    if manifest is None:
         logger.error(f"could not parse xml file '{manifestfile}'")
-        return EX_ERROR
+        return 2
 
     # find list of files in product
-    files = {item for item in product.rglob("*") if item.is_file()}
+    files = [item for item in product.rglob("*") if item.is_file()]
     if manifestfile not in files:
-        logger.error(
-            "could not find 'manifest.safe' in directory listing of product")
-        return EX_ERROR
+        logger.error("could not find 'manifest.safe' in directory listing of product")
+        return 2
     files.remove(manifestfile)
 
     # check files that are referenced in manifest file
     data_objects = {}
     reps = {}
 
-    metadata_section = manifest_xmldoc.find('metadataSection')
+    metadata_section = manifest.find('metadataSection')
     for metadata_object in metadata_section.findall('metadataObject'):
         elem_id = metadata_object.get('ID')
         if elem_id.endswith("Schema"):
@@ -455,42 +412,32 @@ def verify_safe_product(product: os.PathLike,
             if filepath in files:
                 files.remove(filepath)
 
-    information_package_map = manifest_xmldoc.find('informationPackageMap')
-    xp = f"{NSXFDU}contentUnit/{NSXFDU}contentUnit"
-    for content_unit in information_package_map.findall(xp):
-        data_object = content_unit.find('dataObjectPointer')
-        data_object_id = data_object.get('dataObjectID')
+    information_package_map = manifest.find('informationPackageMap')
+    for content_unit in information_package_map.findall(f"{NSXFDU}contentUnit/{NSXFDU}contentUnit"):
+        data_object_id = content_unit.find('dataObjectPointer').get('dataObjectID')
         rep_id = content_unit.get('repID')
         # rep_id is optional (S1-RS-MDA-52-7443 Issue 3.5 10/12/2020)
         if rep_id is not None:
-            # rep_id can be a space separated list of IDs
-            # (first one contains the main schema)
+            # rep_id can be a space separated list of IDs (first one contains the main schema)
             rep_id = rep_id.split()[0]
             if rep_id not in reps:
-                logger.error(
-                    f"dataObject '{data_object_id}' in informationPackageMap "
-                    f"contains repID '{rep_id}' which is not defined in "
-                    f"metadataSection")
-                return EX_ERROR
+                logger.error(f"dataObject '{data_object_id}' in informationPackageMap contains repID '{rep_id}' which "
+                             f"is not defined in metadataSection")
+                return 2
             data_objects[data_object_id] = {'rep': reps[rep_id]}
 
-    data_object_section = manifest_xmldoc.find('dataObjectSection')
+    data_object_section = manifest.find('dataObjectSection')
     for data_object in data_object_section.findall('dataObject'):
         data_object_id = data_object.get('ID')
         if data_object_id not in data_objects:
-            logger.error(
-                f"dataObject '{data_object_id}' in dataObjectSection is "
-                f"not defined in informationPackageMap")
-            return EX_ERROR
+            logger.error(f"dataObject '{data_object_id}' in dataObjectSection is not defined in informationPackageMap")
+            return 2
         rep_id = data_object.get('repID')
-        # rep_id can be a space separated list of IDs
-        # (first one contains the main schema)
+        # rep_id can be a space separated list of IDs (first one contains the main schema)
         rep_id = rep_id.split()[0]
         if data_objects[data_object_id]['rep']['ID'] != rep_id:
-            logger.error(
-                f"dataObject '{data_object_id}' contains repID "
-                f"'{data_objects[data_object_id]['rep']['ID']}' in "
-                f"informationPackageMap, but '{rep_id}' in dataObjectSection")
+            logger.error(f"dataObject '{data_object_id}' contains repID '{data_objects[data_object_id]['rep']['ID']}' "
+                         f"in informationPackageMap, but '{rep_id}' in dataObjectSection")
             has_errors = True
         size = data_object.find('byteStream').get('size')
         href = data_object.find('byteStream/fileLocation').get('href')
@@ -515,16 +462,14 @@ def verify_safe_product(product: os.PathLike,
         # check file size
         filesize = filepath.stat().st_size
         if filesize != int(data_object['size']):
-            logger.error(
-                f"file size for '{filepath}' ({filesize}) does not match "
-                f"file size in manifest.safe ({data_object['size']})")
+            logger.error(f"file size for '{filepath}' ({filesize}) does not match file size in manifest.safe "
+                         f"({data_object['size']})")
             has_errors = True
         # check md5sum
-        checksum = get_md5sum(filepath)
+        checksum = md5sum(filepath)
         if checksum != data_object['checksum']:
-            logger.error(
-                f"checksum for '{filepath}' ({checksum}) does not match "
-                f"checksum in manifest.safe ({data_object['checksum']})")
+            logger.error(f"checksum for '{filepath}' ({checksum}) does not match checksum in manifest.safe "
+                         f"({data_object['checksum']})")
             has_errors = True
         # check with XML Schema (if the file is an xml file)
         if is_xml(filepath) and data_object['rep']:
@@ -545,131 +490,68 @@ def verify_safe_product(product: os.PathLike,
             elif not check_file_against_schema(filepath, schema):
                 has_errors = True
 
-    # Report on files in the SAFE package that are not referenced by the
-    # manifset.safe file
+    # Report on files in the SAFE package that are not referenced by the manifset.safe file
     for filename in sorted(files):
-        logger.warning(
-            f"file '{filename}' found in product but not included "
-            f"in manifest.safe")
+        logger.warning(f"file '{filename}' found in product but not included in manifest.safe")
         has_warnings = True
 
-    # TODO: add consistency checks against the product name
-
     if has_errors:
-        return EX_ERROR
+        return 2
     if has_warnings:
-        return EX_WARNING
-    return EX_OK
-
-
-# --- CLI ---------------------------------------------------------------------
-PROG = 'safechecl'
-LOGFMT = '%(levelname)s: %(message)s'
-
-
-def _autocomplete(parser):
-    try:
-        import argcomplete
-    except ImportError:
-        pass
-    else:
-        argcomplete.autocomplete(parser)
-
-
-def _set_logging_control_args(parser, default_loglevel='WARNING'):
-    """Setup command line options for logging control."""
-    loglevels = [logging.getLevelName(level) for level in range(10, 60, 10)]
-
-    parser.add_argument(
-        '--loglevel', default=default_loglevel, choices=loglevels,
-        help='logging level (default: %(default)s)')
-    parser.add_argument(
-        '-q', '--quiet', dest='loglevel', action='store_const',
-        const='ERROR',
-        help='suppress standard output messages, '
-             'only errors are printed to screen')
-    parser.add_argument(
-        '-v', '--verbose', dest='loglevel', action='store_const',
-        const='INFO', help='print verbose output messages')
-    parser.add_argument(
-        '-d', '--debug', dest='loglevel', action='store_const',
-        const='DEBUG', help='print debug messages')
-
-    return parser
-
-
-def get_parser(subparsers=None):
-    """Instantiate the command line argument (sub-)parser."""
-    name = PROG
-    synopsis = __doc__.splitlines()[0]
-    doc = __doc__
-
-    if subparsers is None:
-        parser = argparse.ArgumentParser(prog=name, description=doc)
-        parser.add_argument(
-            '--version', action='version', version='%(prog)s v' + __version__)
-    else:
-        parser = subparsers.add_parser(name, description=doc, help=synopsis)
-
-    parser = _set_logging_control_args(parser)
-
-    # Command line options
-    parser.add_argument(
-      '-s', '--schema',
-      help='path to the XML schema file for the product manifest')
-
-    # Positional arguments
-    parser.add_argument('products', nargs="+", metavar="SAFE-PRODUCT")
-
-    if subparsers is None:
-        _autocomplete(parser)
-
-    return parser
-
-
-def parse_args(args=None, namespace=None, parser=None):
-    """Parse command line arguments."""
-    if parser is None:
-        parser = get_parser()
-
-    args = parser.parse_args(args, namespace)
-
-    return args
+        return 3
+    return 0
 
 
 def main(*argv):
-    """Main CLI interface."""
-    # setup logging
-    logging.basicConfig(format=LOGFMT, stream=sys.stdout)
+    logging.basicConfig(format='%(levelname)s: %(message)s', stream=sys.stdout)
     logging.captureWarnings(True)
 
-    # parse cmd line arguments
-    args = parse_args(argv if argv else None)
+    # This parser is used in combination with the parse_known_args() function as a way to implement a "--version"
+    # option that prints version information and exits, and is included in the help message.
+    #
+    # The "--version" option should have the same semantics as the "--help" option in that if it is present on the
+    # command line, the corresponding action should be invoked directly, without checking any other arguments.
+    # However, the argparse module does not support user defined options with such semantics.
+    version_parser = argparse.ArgumentParser(add_help=False)
+    version_parser.add_argument('--version', action='store_true', help='output version information and exit')
 
-    # execute main tasks
-    exit_code = EX_OK
+    parser = argparse.ArgumentParser(prog='safecheck', description=__doc__, parents=[version_parser])
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='suppress standard output messages and warnings, only errors are printed to screen')
+    parser.add_argument('-s', '--schema', help='path to the XML schema file for the product manifest')
+    parser.add_argument('products', nargs='+', metavar='<SAFE product>')
+
+    args, unused_args = version_parser.parse_known_args(argv if argv else None)
+    if args.version:
+        print(f'safecheck v{__version__}')
+        print(__copyright__)
+        print()
+        sys.exit(0)
+
+    args = parser.parse_args(unused_args)
+
+    logger.setLevel('ERROR' if args.quiet else 'INFO')
     try:
-        logging.getLogger().setLevel(args.loglevel)
-
-        for product in args.products:
-            print(product)
-            result = verify_safe_product(product, args.schema)
+        return_code = 0
+        for arg in args.products:
+            if not args.quiet:
+                print(arg)
+            result = verify_safe_product(arg, args.schema)
             if result != 0:
-                if result < exit_code or exit_code == EX_OK:
-                    exit_code = result
-            print()
-
+                if result < return_code or return_code == 0:
+                    return_code = result
+            if not args.quiet:
+                print('')
+        return return_code
     except Exception as exc:
         logger.critical(
             'unexpected exception caught: {!r} {}'.format(
                 type(exc).__name__, exc))
         logger.debug('stacktrace:', exc_info=True)
-        exit_code = EX_FAILURE
+        return 1
     except KeyboardInterrupt:
-        logger.warning('Keyboard interrupt received: exit the program')
-        exit_code = EX_INTERRUPT
-
-    return exit_code
+        logger.debug('Keyboard interrupt received: exit the program')
+        return 1
 
 
 if __name__ == '__main__':
